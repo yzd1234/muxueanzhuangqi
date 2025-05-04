8a2a156de446da09b50694c77b6c2229652323a3
元时光 <142206733+yzd1234@users.noreply.github.com>
2025-05-04 13:10:52 +0800

    Delete src directory

--- src/MuXuanZhuangQi.Core/Interfaces/IInstallationStrategy.cs
+++
@@ -1,6 +0,0 @@
-public interface IInstallationStrategy
-{
-    Task<bool> Install(SoftwarePackage package, IProgress<int> progress);
-    Task<bool> Uninstall(string packageId);
-    bool IsSupported(SoftwarePackage package);
-}
--- src/MuXuanZhuangQi.Core/Models/InstallationType.cs
+++
@@ -1,6 +0,0 @@
-public enum InstallationType
-{
-    System,     // 系统安装
-    Shizuku,    // Shizuku方式
-    Root        // Root方式
-}
